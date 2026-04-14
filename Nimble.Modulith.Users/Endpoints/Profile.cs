ea1443051a69df6029667e11acd9e50904bd65a2
Vitalii Novik <novickvitaliy@gmail.com>
2026-04-14 21:05:10 +0300

    finish lab 2

---
+++ Nimble.Modulith.Users/Endpoints/Profile.cs
@@ -0,0 +1,30 @@
+using FastEndpoints;
+using Microsoft.AspNetCore.Identity;
+
+namespace Nimble.Modulith.Users.Endpoints;
+
+public class Profile(SignInManager<IdentityUser> signInManager) : EndpointWithoutRequest<LogoutResponse>
+{
+    private readonly SignInManager<IdentityUser> _signInManager = signInManager;
+
+    public override void Configure()
+    {
+        Post("/profile");
+        AllowAnonymous(); // For now, we'll require auth later when we implement JWT properly
+        Summary(s => {
+            s.Summary = "Profile the current user";
+            s.Description = "Gets the user's profile";
+        });
+    }
+    
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        Response = new LogoutResponse
+        {
+            Success = true,
+            Message = "Profile of the current user"
+        };
+        
+        await Send.OkAsync(Response, ct);
+    }
+}
--- _Host/Nimble.Modulith.Web/Program.cs
+++ _Host/Nimble.Modulith.Web/Program.cs
@@ -1,41 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using FastEndpoints;
+using FastEndpoints.Security;
+using FastEndpoints.Swagger;
+using Nimble.Modulith.Users;
+using Serilog;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var logger = Log.Logger = new LoggerConfiguration()
+  .Enrich.FromLogContext()
+  .WriteTo.Console()
+  .CreateLogger();
+
+logger.Information("Starting web host");
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
+
+builder.AddServiceDefaults();
+
+builder.AddUsersModuleServices(logger);
+
+builder.Services.AddFastEndpoints()
+    .AddAuthenticationJwtBearer(s =>
+    {
+        s.SigningKey = builder.Configuration["Auth:JwtSecret"];
+    })
+    .AddAuthorization()
+    .SwaggerDocument();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+await app.EnsureUsersModuleDatabaseAsync();
 
-app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.UseFastEndpoints()
+    .UseSwaggerGen();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
